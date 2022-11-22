16a5af5dc64b726b2cf3da26c816e9b85f1266cc
LhrAlander <1147816814@qq.com>
2022-11-22 19:37:29 +0800

    基本的快捷键绑定

--- lua/keybindings.lua
+++ lua/keybindings.lua
@@ -12,6 +12,7 @@ map("n", "sj", ":sp<CR>", opt)
 map("n", "sc", "<C-w>c", opt)
 -- close other windows
 map("n", "so", "<C-w>o", opt)
+map("n", "s=", "<C-w>=", opt)
 
 -- Option + hjkl move throw windows
 map("n", "˙", "<C-w>h", opt)
@@ -24,3 +25,14 @@ map("n", "≥", ":vertical resize -20<CR>", opt)
 map("n", "≤", ":vertical resize +20<CR>", opt)
 map("n", "…", ":resize +20<CR>", opt)
 map("n", "æ", ":resize -20<CR>", opt)
+
+-- terminal
+map("n", "<leader>t", ":sp | terminal<CR>", opt)
+map("n", "<leader>lt", ":vsp | terminal<CR>", opt)
+map("t", "<Esc>", "<C-\\><C-n>", opt)
+
+map("v", "<", "<gv", opt)
+map("v", ">", ">gv", opt)
+map("v", "J", ":move '>+1<CR>gv-gv", opt)
+map("v", "K", ":move '<-2<CR>gv-gv", opt)
+
